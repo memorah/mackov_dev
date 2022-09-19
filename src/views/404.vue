087b0701d4371c1b7419c147290272fc5d2a7487
memorah <filip.mackov@gmail.com>
2022-09-19 11:32:12 +0200

    edit

---
+++ src/views/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <MenuItems />
+  </div>
+</template>
+<script>
+import MenuItems from "../components/MenuItems.vue";
+// @ is an alias to /src
+
+export default {
+  components: {
+    MenuItems,
+  },
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "404",
+};
+</script>
+<style scoped>
+.glytch-cancel {
+  display: none;
+}
+</style>
