aef638c5fa72cbabcced024dfbb665e80db47bd9
memorah <filip.mackov@gmail.com>
2022-09-21 07:54:36 +0200

    edit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <BackButton />
+    <ProfileContent />
+  </div>
+</template>
+<script>
+import ProfileContent from "../components/ProfileContent.vue";
+import BackButton from "@/components/BackButton.vue";
+// @ is an alias to /src
+
+export default {
+  components: {
+    ProfileContent,
+    BackButton,
+  },
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "/HomePage",
+};
+</script>
+<style scoped></style>
